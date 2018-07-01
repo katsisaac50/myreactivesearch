5fd180be85724cf73e97db5999c38170cd090fd4
katongole <katsisaac50@gmail.com>
2018-07-01 03:30:06 +0200

    fixed datafied

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <div >
               <CategorySearch
                 componentId="searchbox"
-                dataField="name"
+                dataField="titles"
                 categoryField="titles.raw"
                 placeholder="Search for music"
                 style={{
